d46e5839d4badcede28b5a50f3d197adb75d07e2
Julia Pechnikova <juliape4nikova@yandex.ru>
2022-03-05 00:34:46 +0300

    hotfix/celebration validation

--- middlewares/userValidation.js
+++ middlewares/userValidation.js
@@ -2,21 +2,22 @@ const { celebrate, Joi } = require('celebrate');
 
 module.exports.updateUserInfoValidation = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required(true),
+    name: Joi.string().minlength(2).maxlength(30).required(true),
+    email: Joi.string().email().required(true),
   }),
 });
 
 module.exports.loginValidation = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required(true),
+    email: Joi.string().email().required(true),
     password: Joi.string().required(true),
   }),
 });
 
 module.exports.createUserValidation = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().required(true),
-    email: Joi.string().required(true),
+    name: Joi.string().minlength(2).maxlength(30).required(true),
+    email: Joi.string().email().required(true),
     password: Joi.string().required(true),
   }),
 });
